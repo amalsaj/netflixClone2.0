f2c6e0f5953572458fa1c5faa144c2b8d4e41ac2
amalsaj <amalsaji218@gmail.com>
2024-03-15 20:22:48 +0530

    Functionalty Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import './App.css';
-function App() {
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const Time = new Date().toLocaleString();
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState("");
   return (
     <div className="app">
       <div className="mainHeading">
@@ -11,22 +15,64 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input
+          value={toDo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setTodos([...toDos, { id: Date.now(), text: toDo,time:Time ,status: false }])
+          }
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {toDos.map((value, index) => {
+          return (
+            <div className="todo" key={index}>
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    console.log(e.target.checked);
+                    setTodos(
+                      toDos.filter((value2) => {
+                        if (value2.id === value.id) {
+                          value2.status = e.target.checked;
+                          console.log(value);
+                        }
+                        return value2;
+                      })
+                    );
+                  }}
+                  value={value.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{value.text}</p>
+                <div className="clock">
+                 {value.time}
+                  </div>
+
+              </div>
+              <div className="right">
+                <i
+                  onClick={() => {
+                    // Remove todo when delete icon is clicked
+                    setTodos(toDos.filter((value3) => value3.id !== value.id));
+                    console.log(toDos);
+                  }}
+                  className="fas fa-times"
+                ></i>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
