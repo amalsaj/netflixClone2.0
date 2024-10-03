f02f4f22e92c7a34d31f8be9a769c65d2604da2d
AMAL SAJI <103961626+amalsaj@users.noreply.github.com>
2024-10-03 12:37:30 +0530

    api issue fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import NavBar from './components/NavBar/NavBar'
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
-import Banner from './components/Banner/Banner';
-import RowPost from './components/Row_Post/RowPost';
-import {romance,horror,comedy,action,orginals} from "./constants/urls";
+import Banner from "./components/Banner/Banner";
+import RowPost from "./components/Row_Post/RowPost";
+import { romance, horror, comedy, action, orginals } from "./constants/urls";
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Banner/>
-      <RowPost url={orginals}
-      title='NetFlix Orginals'/>
-      <RowPost url={action} title='Action' isSmall/>
-      <RowPost url={comedy} title='Comedy' isSmall/>
-      <RowPost url={horror} title='Horror' isSmall/>
-      <RowPost url={romance} title='Romance' isSmall/>
+      <NavBar />
+      <Banner />
+      <RowPost url={orginals} title="NetFlix Orginals" />
+      <RowPost url={action} title="Action" isSmall />
+      <RowPost url={comedy} title="Comedy" isSmall />
+      <RowPost url={horror} title="Horror" isSmall />
+      <RowPost url={romance} title="Romance" isSmall />
       {/* <RowPost url={Documentary} title='Documentary' isSmall/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Row_Post/RowPost.js
+++ src/components/Row_Post/RowPost.js
@@ -7,49 +7,55 @@ import { API_KEY } from "../../constants/constants";
 
 function RowPost(props) {
   const [row, SetRow] = useState([]);
-  const [urlId, setID] = useState('');
-  
+  const [urlId, setID] = useState("");
+
   useEffect(() => {
     axios.get(props.url).then((response) => {
       SetRow(response.data.results);
-    }, []);
-  });
+    });
+  }, [props.url]); // Add props.url as a dependency to ensure data is fetched when it changes.
+
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
-  const handleMovie=(id)=>{
-    axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&with_genres=99`).then((response)=>{
-      if(response.data.results.length!==0){
-        setID(response.data.results[0])
-      }
 
-    })
+  const handleMovie = (id) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&with_genres=99`
+      )
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setID(response.data.results[0]); // Ensure urlId.key exists in the object.
+        } else {
+          console.log("No videos available");
+        }
+      });
+  };
 
-
-  }
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
         {row.map((obj) => {
           return (
-            <figure>
-              <img onClick={()=>handleMovie(obj.id)}
+            <figure key={obj.id}>
+              <img
+                onClick={() => handleMovie(obj.id)}
                 className={props.isSmall ? "samllPoster" : "poster"}
-                src={`${imageUrl + obj.backdrop_path}`}
-                alt="card"
+                src={obj.backdrop_path ? `${imageUrl + obj.backdrop_path}` : "fallback-image-url"} // Provide fallback image if path is null.
+                alt={obj.original_title || "Movie poster"}
               />
               <figcaption>{obj.original_title}</figcaption>
             </figure>
           );
         })}
       </div>
-     { urlId && <YouTube videoId={urlId.key} opts={opts}/>}
+      {urlId && <YouTube videoId={urlId.key} opts={opts} />} {/* Ensure urlId has a key */}
     </div>
   );
 }
